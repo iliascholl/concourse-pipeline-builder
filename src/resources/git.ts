35fed6b50ae896b279abf60d3ac1c9ed39a099f4
Ilia Belozerov <ilia.belozerov@digitalunit.berlin>
2019-11-17 14:25:32 +0100

    Align naming of parameter object

--- src/resources/git.ts
+++ src/resources/git.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { ResourceWrapper } from '../builder';
 
-export type GitResourceParams = {
+export type GitParams = {
     uri: string;
     name?: string;
     private_key?: string;
     branch?: string;
 };
 
-export function git({ uri, name, private_key, branch }: GitResourceParams): ResourceWrapper {
+export function git({ uri, name, private_key, branch }: GitParams): ResourceWrapper {
     return {
         resource: {
             name: name || 'source',
